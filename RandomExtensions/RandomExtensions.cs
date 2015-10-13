78ee0342fd4111f22c1cb2f62a02ac7f559f6cc9
Herman Groenenboom <taartaar@gmail.com>
2015-10-13 11:54:16 +0200

    Added Matrix rotations, Vector rotations, improved Choose with params.

--- RandomExtensions/RandomExtensions.cs
+++ RandomExtensions/RandomExtensions.cs
@@ -13,18 +13,38 @@ namespace RandomExtensions
         public static Vector3 NextVector3Unit(this Random r) => Vector3.Normalize(new Vector3(r.NextFloat(), r.NextFloat(), r.NextFloat()));
 
         public static Color NextColor(this Random r) => new Color(NextVector3Unit(r));
+        
+        public static Matrix NextRotationX(this Random r) => Matrix.CreateRotationX(r.NextFloat()*MathHelper.TwoPi);
+        public static Matrix NextRotationY(this Random r) => Matrix.CreateRotationY(r.NextFloat()*MathHelper.TwoPi);
+        public static Matrix NextRotationZ(this Random r) => Matrix.CreateRotationZ(r.NextFloat()*MathHelper.TwoPi);
+
+        /// <summary>
+        /// Rotates a Vector2 to a random direction
+        /// </summary>
+        public static Vector2 NextRotateVector2(this Random r, Vector2 v) => r.NextVector2Unit()*v.Length();
+
+        /// <summary>
+        /// Rotates a Vector3 to a random direction
+        /// </summary>
+        public static Vector3 NextRotateVector3(this Random r, Vector3 v) => r.NextVector3Unit()*v.Length();
 
         #endregion MonoGame
 
         #region Collections
 
-        public static T Choose<T>(this Random rand, IList<T> list) => list[rand.Next(list.Count)];
+        /// <summary>
+        /// Chooses a random element from a given list.
+        /// </summary>
+        public static T Choose<T>(this Random r, IList<T> list) => list[r.Next(list.Count)];
 
-        public static T Choose<T>(this Random rand, T[] items) => items[rand.Next(items.Length)];
+        /// <summary>
+        /// Chooses a random element from a given array or parameters.
+        /// </summary>
+        public static T Choose<T>(this Random r, params T[] items) => items[r.Next(items.Length)];
 
         #endregion Collections
 
-        public static float NextFloat(this Random rand) => (float) rand.NextDouble();
+        public static float NextFloat(this Random r) => (float) r.NextDouble();
 
     }
 }
