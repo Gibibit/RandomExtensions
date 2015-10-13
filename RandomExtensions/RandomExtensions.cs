5afe1218d612a42bfaa36f20eada33192f06e35a
Herman Groenenboom <taartaar@gmail.com>
2015-10-13 11:40:52 +0200

    Put it all in one project for now, can always create seperate assemblies later. Added Vector3 unit Added Color Added array Choose Added float

--- RandomExtensions/RandomExtensions.cs
+++ RandomExtensions/RandomExtensions.cs
@@ -1,10 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.Xna.Framework;
 
-namespace RandomExtensions.System
+namespace RandomExtensions
 {
-    public static class RandomExtensions
+    public static class MonoGameRandomExtensions
     {
-        public static T Choose<T>(this Random rand, List<T> list) => list[rand.Next(list.Count)];
+        #region MonoGame
+
+        public static Vector2 NextVector2Unit(this Random r) => Vector2.Normalize(new Vector2(r.NextFloat(), r.NextFloat()));
+
+        public static Vector3 NextVector3Unit(this Random r) => Vector3.Normalize(new Vector3(r.NextFloat(), r.NextFloat(), r.NextFloat()));
+
+        public static Color NextColor(this Random r) => new Color(NextVector3Unit(r));
+
+        #endregion MonoGame
+
+        #region Collections
+
+        public static T Choose<T>(this Random rand, IList<T> list) => list[rand.Next(list.Count)];
+
+        public static T Choose<T>(this Random rand, T[] items) => items[rand.Next(items.Length)];
+
+        #endregion Collections
+
+        public static float NextFloat(this Random rand) => (float) rand.NextDouble();
+
     }
 }
