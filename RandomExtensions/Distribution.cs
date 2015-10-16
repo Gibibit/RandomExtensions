577a991fd898b12481d7814ad01456ee43cb9fe1
Herman Groenenboom <taartaar@gmail.com>
2015-10-16 14:08:51 +0200

    Moved Distribution and Range to their own files.

---
+++ RandomExtensions/Distribution.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+
+namespace RandomExtensions
+{
+    public class Distribution<T>
+    {
+        private Random _rand;
+        private Dictionary<Range, T> _ranges;
+        private Dictionary<T, int> _distribution; 
+
+        internal Distribution(Random rand, Dictionary<T, int> distribution)
+        {
+            _rand = rand;
+            _distribution = distribution;
+            _ranges = DistributionToRanges(_distribution);
+        }
+
+        [Pure]
+        private static Dictionary<Range, T> DistributionToRanges(Dictionary<T, int> distribution)
+        {
+            var ranges = new Dictionary<Range, T>();
+
+            int total = 0;
+            foreach (var kv in distribution) {
+                var min = total;
+                total += kv.Value;
+                var max = total;
+                ranges.Add(new Range(min, max), kv.Key);
+            }
+
+            return ranges;
+        } 
+
+        public T NextValue()
+        {
+            var max = _ranges.Max(kv => kv.Key.Max);
+            var selection = _rand.Next(max);
+
+            return _ranges.First(kv => kv.Key.Min <= selection && kv.Key.Max > selection).Value;
+        }
+    }
+
+    internal struct Range
+    {
+        public int Min { get; }
+        public int Max { get; }
+
+        public Range(int min, int max)
+        {
+            Min = min;
+            Max = max;
+        }
+    }
+}
--- RandomExtensions/RandomExtensions.cs
+++ RandomExtensions/RandomExtensions.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Contracts;
-using System.Linq;
 using Microsoft.Xna.Framework;
 
 namespace RandomExtensions
@@ -105,55 +103,5 @@ namespace RandomExtensions
         /// <param name="distribution">Dictionary of values and frequencies with Dictionary keys acting as values and Dictionary values acting as frequencies.</param>
         /// <returns>An instance of Distribution which returns the specified values in the specified frequencies.</returns>
         public static Distribution<T> NextDistribution<T>(this Random rand, Dictionary<T, int> distribution) => new Distribution<T>(rand, distribution);
-    }
-
-    public class Distribution<T>
-    {
-        private Random _rand;
-        private Dictionary<Range, T> _ranges;
-        private Dictionary<T, int> _distribution; 
-
-        internal Distribution(Random rand, Dictionary<T, int> distribution)
-        {
-            _rand = rand;
-            _distribution = distribution;
-            _ranges = DistributionToRanges(_distribution);
-        }
-
-        [Pure]
-        private static Dictionary<Range, T> DistributionToRanges(Dictionary<T, int> distribution)
-        {
-            var ranges = new Dictionary<Range, T>();
-
-            int total = 0;
-            foreach (var kv in distribution) {
-                var min = total;
-                total += kv.Value;
-                var max = total;
-                ranges.Add(new Range(min, max), kv.Key);
-            }
-
-            return ranges;
-        } 
-
-        public T NextValue()
-        {
-            var max = _ranges.Max(kv => kv.Key.Max);
-            var selection = _rand.Next(max);
-
-            return _ranges.First(kv => kv.Key.Min <= selection && kv.Key.Max > selection).Value;
-        }
-    }
-
-    internal struct Range
-    {
-        public int Min { get; }
-        public int Max { get; }
-
-        public Range(int min, int max)
-        {
-            Min = min;
-            Max = max;
-        }
     }
 }
