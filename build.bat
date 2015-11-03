5c93a0461d23cc6c0b68533af7bff7780a11becf
Herman Groenenboom <taartaar@gmail.com>
2015-11-03 17:43:48 +0100

    Fixed build.bat

--- build.bat
+++ build.bat
@@ -20,4 +20,4 @@ mkdir Build
 mkdir Build\lib
 mkdir Build\lib\net40
 
-%nuget% pack ".nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
+%nuget% pack "randomextensions.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version %version% -p Configuration="%config%"
